288ca2450585754bd0b231da58144055d0e54adb
Ramin <raminramazanpour@gmail.com>
2024-08-08 01:57:16 +0330

    test

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ const server = Bun.serve({
     port: 3000,
     async fetch(req) {
         try {
-            return new Response("Welcome to my Farcaster praise bot!!!");
+            return new Response("Welcome to my Farcaster praise bot!!!!!");
         } catch (e: any) {
             return new Response(e.message, { status: 500 });
         }
