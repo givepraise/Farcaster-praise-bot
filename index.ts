8e506d08783b817cd6feedaf67a49d4d6b4a64e6
Ramin <raminramazanpour@gmail.com>
2024-08-09 20:01:15 +0330

    add reply to mentions

--- index.ts
+++ index.ts
@@ -1,7 +1,24 @@
+import neynarClient from "./neynarClient";
+
 const server = Bun.serve({
     port: 3000,
     async fetch(req) {
         try {
+            if (!process.env.SIGNER_UUID) {
+                throw new Error("Make sure you set SIGNER_UUID in your .env file");
+            }
+
+            const body = await req.text();
+            const hookData = JSON.parse(body);
+
+            const reply = await neynarClient.publishCast(
+                process.env.SIGNER_UUID,
+                `gm ${hookData.data.author.username}`,
+                {
+                    replyTo: hookData.data.hash,
+                }
+            );
+            console.log("reply:", reply);
             return new Response("Welcome to my Farcaster praise bot!!!!!");
         } catch (e: any) {
             return new Response(e.message, { status: 500 });
