29eb7f9f4dc77dc37a0288d1c9a13b522e796c14
Ramin <raminramazanpour@gmail.com>
2024-08-10 19:59:40 +0330

    add frame to reply

--- index.ts
+++ index.ts
@@ -1,25 +1,53 @@
 import neynarClient from "./neynarClient";
+import type {NeynarFrameCreationRequest} from "@neynar/nodejs-sdk/build/neynar-api/v2";
 
 const server = Bun.serve({
     port: 3000,
     async fetch(req) {
         try {
+            const body = await req.text();
+            const hookData = JSON.parse(body);
+
+            const creationRequest: NeynarFrameCreationRequest = {
+                name: `gm ${hookData.data.author.username}`,
+                pages: [
+                    {
+                        image: {
+                            url: "https://moralis.io/wp-content/uploads/web3wiki/638-gm/637aeda23eca28502f6d3eae_61QOyzDqTfxekyfVuvH7dO5qeRpU50X-Hs46PiZFReI.jpeg",
+                            aspect_ratio: "1:1",
+                        },
+                        title: "Page title",
+                        buttons: [],
+                        input: {
+                            text: {
+                                enabled: false,
+                            },
+                        },
+                        uuid: "gm",
+                        version: "vNext",
+                    },
+                ],
+            };
+            const frame = await neynarClient.publishNeynarFrame(creationRequest);
+
             if (!process.env.SIGNER_UUID) {
                 throw new Error("Make sure you set SIGNER_UUID in your .env file");
             }
-
-            const body = await req.text();
-            const hookData = JSON.parse(body);
 
             const reply = await neynarClient.publishCast(
                 process.env.SIGNER_UUID,
                 `gm ${hookData.data.author.username}`,
                 {
+                    embeds: [
+                        {
+                            url: frame.link,
+                        },
+                    ],
                     replyTo: hookData.data.hash,
                 }
             );
-            console.log("reply:", reply);
-            return new Response("Welcome to my Farcaster praise bot!!!!!");
+            console.log("Date:" + new Date() + reply);
+            return new Response(`Replied to the cast with hash: ${reply.hash}`);
         } catch (e: any) {
             return new Response(e.message, { status: 500 });
         }
